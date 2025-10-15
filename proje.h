a3ac0a99f54e92e32617c92cfa68248a6c13489e
Bora Akın <s240307045@ankarabilim.edu.tr>
2025-10-15 13:52:17 +0300

    Update proje.h

---
+++ proje.h
@@ -0,0 +1,17 @@
+/* proje.h
+   Bu dosya örnek bir başlık dosyasıdır. Workspace'te boş olduğu için
+   derlemeye/çalıştırmaya engel oluyordu. Eğer C/C++ projeniz varsa
+   buraya gerekli bildirimleri ekleyin.
+
+   Aşağıda basit bir include guard ve küçük bir helper bildirimi bulunur.
+*/
+
+#ifndef PROJE_H
+#define PROJE_H
+
+/* Örnek: küçük bir yardımcı fonksiyon bildirimi */
+static inline int proje_topla(int a, int b) {
+	return a + b;
+}
+
+#endif /* PROJE_H */
